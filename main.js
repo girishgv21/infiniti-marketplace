d432c409cf666548ceb060ec3328b932433096b3
girishgv21 <girishgv21@gmail.com>
2023-02-17 13:06:16 +0530

    Site updates

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(function() {
-    console.log('Working')
+    // Header Scrolling Effects
     let headerHeight = jQuery('header').outerHeight();
     if (window.matchMedia('(min-width: 1025px)').matches) {
         $(window).on("scroll", function () {
@@ -23,16 +23,26 @@ $(function() {
         });
     }
 
+    // Mobile Menu
     $('.mobile-menu').on('click', function() {
         $(this).toggleClass('active')
         $('.header-nav').toggleClass('active')
         $('body').toggleClass('overlay')
     })
 
+    // Close menu on mobile link click
     $('header li a').on('click', function() {
         $('.mobile-menu').removeClass('active')
         $('.header-nav').removeClass('active')
         $('body').removeClass('overlay')
+    })
+
+    // Category filters
+    $('.product-categories li a').on('click', function() {
+        let dataValue = $(this).data('value')
+        $('.product-categories li a').removeClass('active')
+        $(this).addClass('active')
+        $('#products .product-heading h3').text(dataValue)
     })
     
 })
