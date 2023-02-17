60c42a75cdde3cf6246a3d0795cd3c55a268523f
girishgv21 <girishgv21@gmail.com>
2023-02-17 12:53:37 +0530

    JS Updates Mobile

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
 $(function() {
     console.log('Working')
     let headerHeight = jQuery('header').outerHeight();
-
-    $(window).on("scroll", function () {
-        if ($(this).scrollTop() > headerHeight) {
-            $("header").addClass("active");
-        } else {
-            $("header").removeClass("active");
-        }
-        
-    });
-
-    var lastScrollTop = 0;
-    $(window).scroll(function(event){
-        var st = $(this).scrollTop();
-        if (st > lastScrollTop){
-            $('header').addClass('hide');
-        } else {
-            $('header').removeClass('hide');
-        }
-        lastScrollTop = st;
-    });
+    if (window.matchMedia('(min-width: 1025px)').matches) {
+        $(window).on("scroll", function () {
+            if ($(this).scrollTop() > headerHeight) {
+                $("header").addClass("active");
+            } else {
+                $("header").removeClass("active");
+            }
+            
+        });
+    
+        var lastScrollTop = 0;
+        $(window).scroll(function(event){
+            var st = $(this).scrollTop();
+            if (st > lastScrollTop){
+                $('header').addClass('hide');
+            } else {
+                $('header').removeClass('hide');
+            }
+            lastScrollTop = st;
+        });
+    }
 
     $('.mobile-menu').on('click', function() {
         $(this).toggleClass('active')
